d1aff79fb3819de80c61300fe47de86b1e69e410
AbdoullahBougataya <sience.story@gmail.com>
2025-01-01 15:50:07 +0000

    Jan 1, 2025, 6:50:07 PM

--- Flight_controller.ino
+++ Flight_controller.ino
@@ -4,7 +4,7 @@ BMI160 bmi160;
 const int8_t addr = 0x68;
 
 #define G_MPS2 9.81000000000000000000f // g
-float alpha = 0.75;
+float alpha = 0.01;
 
 void setup(){
   Serial.begin(115200);
@@ -26,20 +26,21 @@ void setup(){
 void loop(){
   int rslt;
   int16_t accelGyro[6]={0};
-  int16_t filteredAccelGyro[6]={0};
+  float filteredAccelGyro[6]={0};
+  float rawAccelGyro[6]={0};
   //get both accel and gyro data from bmi160
   //parameter accelGyro is the pointer to store the data
   rslt = bmi160.getAccelGyroData(accelGyro);
   if(rslt == 0){
-    accelGyro[0] = (accelGyro[0] + 9) / 16.4;
-    accelGyro[1] = (accelGyro[1] - 4) / 16.4;
-    accelGyro[2] = (accelGyro[2] - 7) / 16.4;
-    accelGyro[3] = ((accelGyro[3] / 16384.0) - 0.03);
-    accelGyro[4] = ((accelGyro[4] / 16384.0) + 0.03);
-    accelGyro[5] = ((accelGyro[5] / 16384.0) - 0.03);
+    rawAccelGyro[0] = (accelGyro[0] + 9) / 16.4;
+    rawAccelGyro[1] = (accelGyro[1] - 4) / 16.4;
+    rawAccelGyro[2] = (accelGyro[2] - 7) / 16.4;
+    rawAccelGyro[3] = ((accelGyro[3] / 16384.0) - 0.03);
+    rawAccelGyro[4] = ((accelGyro[4] / 16384.0) + 0.03);
+    rawAccelGyro[5] = ((accelGyro[5] / 16384.0) - 0.03);
     for(int i = 0; i < 6; i++)
     {
-      filteredAccelGyro[i] = alpha * accelGyro[i] + (1 - alpha) * filteredAccelGyro[i]; // EMA Filter
+      filteredAccelGyro[i] = 12.5 * (alpha * rawAccelGyro[i] + (1 - alpha) * filteredAccelGyro[i]); // EMA Filter
       Serial.print(filteredAccelGyro[i]);Serial.print("\t");
     }
     Serial.println();
@@ -48,7 +49,3 @@ void loop(){
   }
   delay(50);
 }
-
-/*
-
-*/
