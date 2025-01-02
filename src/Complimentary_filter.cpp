09c330ff49a41c355dc0fae8415558986969ce8e
AbdoullahBougataya <sience.story@gmail.com>
2025-01-02 18:00:18 +0000

    Jan 2, 2025, 9:00:18 PM

--- src/Complimentary_filter.cpp
+++ src/Complimentary_filter.cpp
@@ -1,3 +1,8 @@
 #include"../include/Complimentary_filter.h"
 
-
+void complimentaryFilter(int p, int q, int r, double x_acc, double y_acc, double z_acc, int &phi, double &theta)
+{
+    // Using gravity to estimate the euler angles
+    phiHat_acc_rad = atanf(filteredAccelGyro[4] / filteredAccelGyro[5]); // Roll estimate
+    thetaHat_acc_rad = asinf(fminf(fmaxf(filteredAccelGyro[3] / G_MPS2, -1.0f), 1.0f)); // Pitch estimate
+}
