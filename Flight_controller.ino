0a5d240b7680cd7a6baf3e8dee46d26c76ac5ebf
AbdoullahBougataya <sience.story@gmail.com>
2025-01-02 18:04:36 +0000

    Jan 2, 2025, 9:04:36 PM

--- Flight_controller.ino
+++ Flight_controller.ino
@@ -70,7 +70,7 @@ void loop(){
     float phiDot_rps = filteredAccelGyro[0] + tanf(thetaHat_rad) * (sinf(phiHat_rad) * filteredAccelGyro[1] + cosf(phiHat_rad) * filteredAccelGyro[2]); // Roll rate (rad/s)
     float thetaDot_rps =                                            cosf(phiHat_rad) * filteredAccelGyro[1] - sinf(phiHat_rad) * filteredAccelGyro[2]; // Pitch rate (rad/s)
     // Complementary filter implementation
-    phiHat_rad = COMP_FLTR_ALPHA * phiHat_acc_rad + (1.0f - COMP_FLTR_ALPHA) * (phiHat_rad + dt * phiDot_rps); // Roll estimate
+    phiHat_rad = COMP_FLTR_ALPHA * atanf(filteredAccelGyro[4] / filteredAccelGyro[5]) + (1.0f - COMP_FLTR_ALPHA) * (phiHat_rad + dt * (filteredAccelGyro[0] + tanf(thetaHat_rad) * (sinf(phiHat_rad) * filteredAccelGyro[1] + cosf(phiHat_rad) * filteredAccelGyro[2]))); // Roll estimate
     thetaHat_rad = COMP_FLTR_ALPHA * thetaHat_acc_rad + (1.0f - COMP_FLTR_ALPHA) * (thetaHat_rad + dt * thetaDot_rps); // Pitch estimate
     Serial.print("Roll-estimate:");
     Serial.print(phiHat_rad * RAD2DEG);
--- src/Complimentary_filter.cpp
+++ src/Complimentary_filter.cpp
@@ -1,8 +1,9 @@
 #include"../include/Complimentary_filter.h"
 
-void complimentaryFilter(int p, int q, int r, double x_acc, double y_acc, double z_acc, int &phi, double &theta)
+void complimentaryFilter(float p, float q, float r, float x_acc, float y_acc, float z_acc, float deltaTime, float &phi, float &theta)
 {
     // Using gravity to estimate the euler angles
     phiHat_acc_rad = atanf(filteredAccelGyro[4] / filteredAccelGyro[5]); // Roll estimate
     thetaHat_acc_rad = asinf(fminf(fmaxf(filteredAccelGyro[3] / G_MPS2, -1.0f), 1.0f)); // Pitch estimate
+    phi = COMP_FLTR_ALPHA * atanf(filteredAccelGyro[4] / filteredAccelGyro[5]) + (1.0f - COMP_FLTR_ALPHA) * (phiHat_rad + deltaTime * (filteredAccelGyro[0] + tanf(thetaHat_rad) * (sinf(phiHat_rad) * filteredAccelGyro[1] + cosf(phiHat_rad) * filteredAccelGyro[2])));
 }
