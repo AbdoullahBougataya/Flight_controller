5d9b5025f2ae1e71370c7351d46156848b7964a3
AbduSOBITKHANOV <new1687ton@gmail.com>
2025-01-02 21:07:34 +0300

    Update By ST

--- Flight_controller.ino
+++ Flight_controller.ino
@@ -4,74 +4,82 @@
 BMI160 bmi160;
 const int8_t addr = 0x68;
 
-#define G_MPS2 9.81000000000000000000f // g
-#define ALPHA 0.02000000000000000000f // Filter coefficient
-#define COMP_FLTR_ALPHA 0.50000000000000000000f // Complementary filter coefficient
-#define DPS2RPS 0.01745329251994329576f // degrees to radians per second
-#define RAD2DEG 57.2957795130823208767f // Radians to degrees
+#define G_MPS2          9.81000000000000000000f   // Gravitational acceleration (g)
+#define ALPHA           0.02000000000000000000f   // Filter coefficient
+#define COMP_FLTR_ALPHA 0.50000000000000000000f   // Complementary filter coefficient
+#define DPS2RPS         0.01745329251994329576f   // Degrees to radians (per second)
+#define RAD2DEG         57.2957795130823208767f   // Radians to degrees (per second)
 
 // Define the time step
 float dt = 0.0;
 static unsigned long lastTime = 0;
+static unsigned long currentTime = 0;
 
 float phiHat_acc_rad = 0.0f;
 float thetaHat_acc_rad = 0.0f;
 float phiHat_rad = 0.0f;
 float thetaHat_rad = 0.0f;
 
-int16_t accelGyro[6]={0};
-float filteredAccelGyro[6]={0};
-float rawAccelGyro[6]={0};
-
-void setup(){
+void setup() {
   Serial.begin(115200);
   delay(100);
 
-  //init the hardware bmin160
-  if (bmi160.softReset() != BMI160_OK){
+  // Initialize the hardware bmin160
+  if (bmi160.softReset() != BMI160_OK) {
     Serial.println("reset false");
-    while(1);
+    while (1);
   }
 
-  //set and init the bmi160 i2c address
-  if (bmi160.Init(addr) != BMI160_OK){
+  // Set and init the bmi160 i2c address
+  if (bmi160.Init(addr) != BMI160_OK) {
     Serial.println("init false");
-    while(1);
+    while (1)
+      ;
   }
 }
-void loop(){
-  unsigned long currentTime = millis();
-  dt = (currentTime - lastTime) / 1000.0; // Time in seconds
+
+void loop() {
+  // Calculate time stamp (in seconds)
+  currentTime = millis() / 1000.0;
+  dt = (currentTime - lastTime);
   lastTime = currentTime;
-  int rslt;
+
+  int16_t accelGyro[6] = { 0 };
+  float filteredAccelGyro[6] = { 0 };
+  float rawAccelGyro[6] = { 0 };
+  
   //get both accel and gyro data from bmi160
   //parameter accelGyro is the pointer to store the data
-  rslt = bmi160.getAccelGyroData(accelGyro);
-  if(rslt == 0){
+  int rslt = bmi160.getAccelGyroData(accelGyro);
+  
+  if (rslt == 0) {
     rawAccelGyro[0] = (accelGyro[0] + 9) * DPS2RPS;
     rawAccelGyro[1] = (accelGyro[1] - 4) * DPS2RPS;
     rawAccelGyro[2] = (accelGyro[2] - 7) * DPS2RPS;
     rawAccelGyro[3] = ((accelGyro[3] / 16384.0) - 0.03) * G_MPS2;
     rawAccelGyro[4] = ((accelGyro[4] / 16384.0) + 0.03) * G_MPS2 + 0.4;
     rawAccelGyro[5] = ((accelGyro[5] / 16384.0) - 0.03) * G_MPS2;
-    for(int i = 0; i < 6; i++)
-    {
-      if (rawAccelGyro[i] <= 0.3 && rawAccelGyro[i] >= -0.2)
-      {
+    for (int i = 0; i < 6; i++) {
+      if (rawAccelGyro[i] <= 0.3 && rawAccelGyro[i] >= -0.2) {
         rawAccelGyro[i] = 0;
       }
-      filteredAccelGyro[i] = 50 * (ALPHA * rawAccelGyro[i] + (1 - ALPHA) * filteredAccelGyro[i]); // Low-pass EMA Filter
-      Serial.print(filteredAccelGyro[i]);Serial.print("\t");
+      filteredAccelGyro[i] = 50 * (ALPHA * rawAccelGyro[i] + (1 - ALPHA) * filteredAccelGyro[i]);  // Low-pass EMA Filter
+      Serial.print(filteredAccelGyro[i]);
+      Serial.print("\t");
     }
+    
     // Using gravity to estimate the euler angles
-    phiHat_acc_rad = atanf(filteredAccelGyro[4] / filteredAccelGyro[5]); // Roll estimate
-    thetaHat_acc_rad = asinf(fminf(fmaxf(filteredAccelGyro[3] / G_MPS2, -1.0f), 1.0f)); // Pitch estimate
+    phiHat_acc_rad = atanf(filteredAccelGyro[4] / filteredAccelGyro[5]);                 // Roll estimate
+    thetaHat_acc_rad = asinf(fminf(fmaxf(filteredAccelGyro[3] / G_MPS2, -1.0f), 1.0f));  // Pitch estimate
+    
     // Using gyroscope to estimate the euler rates
-    float phiDot_rps = filteredAccelGyro[0] + tanf(thetaHat_rad) * (sinf(phiHat_rad) * filteredAccelGyro[1] + cosf(phiHat_rad) * filteredAccelGyro[2]); // Roll rate (rad/s)
-    float thetaDot_rps =                                            cosf(phiHat_rad) * filteredAccelGyro[1] - sinf(phiHat_rad) * filteredAccelGyro[2]; // Pitch rate (rad/s)
+    float phiDot_rps = filteredAccelGyro[0] + tanf(thetaHat_rad) * (sinf(phiHat_rad) * filteredAccelGyro[1] + cosf(phiHat_rad) * filteredAccelGyro[2]);  // Roll rate (rad/s)
+    float thetaDot_rps = cosf(phiHat_rad) * filteredAccelGyro[1] - sinf(phiHat_rad) * filteredAccelGyro[2];                                              // Pitch rate (rad/s)
+    
     // Complementary filter implementation
-    phiHat_rad = COMP_FLTR_ALPHA * phiHat_acc_rad + (1.0f - COMP_FLTR_ALPHA) * (phiHat_rad + dt * phiDot_rps); // Roll estimate
-    thetaHat_rad = COMP_FLTR_ALPHA * thetaHat_acc_rad + (1.0f - COMP_FLTR_ALPHA) * (thetaHat_rad + dt * thetaDot_rps); // Pitch estimate
+    phiHat_rad = COMP_FLTR_ALPHA * phiHat_acc_rad + (1.0f - COMP_FLTR_ALPHA) * (phiHat_rad + dt * phiDot_rps);          // Roll estimate
+    thetaHat_rad = COMP_FLTR_ALPHA * thetaHat_acc_rad + (1.0f - COMP_FLTR_ALPHA) * (thetaHat_rad + dt * thetaDot_rps);  // Pitch estimate
+
     Serial.print("Roll-estimate:");
     Serial.print(phiHat_rad * RAD2DEG);
     Serial.print("\t");
@@ -79,7 +87,8 @@ void loop(){
     Serial.print(thetaHat_rad * RAD2DEG);
     Serial.print("\t");
     Serial.println();
-  }else{
+  }
+  else {
     Serial.println("err");
   }
   delay(50);
