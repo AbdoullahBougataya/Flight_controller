f3725765af93121c8f482784725960a02c8b1b31
AbdoullahBougataya <sience.story@gmail.com>
2025-01-03 08:22:32 +0000

    Jan 3, 2025, 11:22:32 AM

--- include/Filter.h
+++ include/Filter.h
@@ -1,3 +1,4 @@
 #include <Arduino.h>
 #define COMP_FLTR_ALPHA 0.50000000000000000000f // Complimentary filter coefficient
 #define EMA_ALPHA 0.01000000000000000000f // EMA filter coefficient
+G_MPS2
