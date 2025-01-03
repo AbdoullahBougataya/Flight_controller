9afdcb2efe35f66b552669eedfe0110f7146b82b
AbdoullahBougataya <sience.story@gmail.com>
2025-01-03 14:11:38 +0000

    Jan 3, 2025, 5:11:38 PM

--- include/Filter.h
+++ include/Filter.h
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 #define COMP_FLTR_ALPHA 0.05000000000000000000f // Complimentary filter coefficient
-#define EMA_ALPHA       0.01000000000000000000f // EMA filter coefficient
+#define EMA_ALPHA       0.50000000000000000000f // EMA filter coefficient
 #define G_MPS2          9.81000000000000000000f // Gravitational acceleration (g)
 
 /**
