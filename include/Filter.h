b5029c76cf3b47aea5d23c9e3b29687f72bdfcf1
AbdoullahBougataya <sience.story@gmail.com>
2025-01-03 08:39:14 +0000

    Jan 3, 2025, 11:39:14 AM

--- include/Filter.h
+++ include/Filter.h
@@ -3,3 +3,15 @@
 #define COMP_FLTR_ALPHA 0.50000000000000000000f // Complimentary filter coefficient
 #define EMA_ALPHA       0.01000000000000000000f // EMA filter coefficient
 #define G_MPS2          9.81000000000000000000f // Gravitational acceleration (g)
+
+/**
+* @fn complimentaryFilter
+* @brief Apply a complimentary filter
+* @param type  three type
+* @n     onlyAccel    :   only get the accel data
+* @n     onlyGyro     :   only get the gyro data
+* @n     bothAccelGyro:   get boath accel and gyro data
+* @param data  save returned data to parameter data
+* @return BMI160_OK(0) means succse
+*/
+void complimentaryFilter(float* filteredAccelGyro, float phiHat_rad, float thetaHat_rad, float dt);
