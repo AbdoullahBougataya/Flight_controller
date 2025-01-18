9e1012a4d9448fd9bf8d67481de5cda2a7b0af2f
AbdoullahBougataya <sience.story@gmail.com>
2025-01-18 07:32:48 +0000

    Jan 18, 2025, 10:32:48 AM

--- include/Filter.h
+++ include/Filter.h
@@ -1,23 +1,3 @@
 #include <Arduino.h>
 
-#define COMP_FLTR_ALPHA 0.01000000000000000000f // Complimentary filter coefficient
-#define EMA_ALPHA       0.80000000000000000000f // EMA filter coefficient
 #define G_MPS2          9.81000000000000000000f // Gravitational acceleration (g)
-
-/**
-  * @fn complimentaryFilter
-  * @brief Apply a complimentary filter
-  * @param filteredAccelGyro Filtered input signal array in (m/s²) and (rad/s)
-  * @param phiHat_rad  Output pitch angle of the complimentary filter in (rad)
-  * @param thetaHat_rad  Output roll angle of the complimentary filter in (rad)
-  * @param dt  Sampling period
-  */
-void complimentaryFilter(float* filteredAccelGyro, float &phiHat_rad, float &thetaHat_rad, float dt);
-
-/**
-  * @fn EMAFilter
-  * @brief Apply an EMA Filter to the data
-  * @param rawAccelGyro Raw data in (m/s²) and (rad/s)
-  * @param filteredAccelGyro  EMA Filtered signal in (m/s²) and (rad/s)
-  */
-void EMAFilter(float* rawAccelGyro, float* filteredAccelGyro);
--- src/Filter.cpp
+++ src/Filter.cpp
@@ -1,28 +1,3 @@
 #include"../include/Filter.h"
 #include <math.h>
 
-// Low-pass EMA Filter
-void EMAFilter(float* rawAccelGyro, float* filteredAccelGyro) {
-  for (uint8_t i = 0; i < 6; i++) {
-    // Default to zero in low amplitude noise
-    if (rawAccelGyro[i] <= 0.3 && rawAccelGyro[i] >= -0.2) {
-      rawAccelGyro[i] = 0;
-    }
-    filteredAccelGyro[i] = EMA_ALPHA * rawAccelGyro[i] + (1 - EMA_ALPHA) * filteredAccelGyro[i];
-  }
-}
-
-// Complimentary filter
-void complimentaryFilter(float* filteredAccelGyro, float &phiHat_rad, float &thetaHat_rad, float dt) {
-  // Using gravity to estimate the Euler angles
-  float phiHat_acc_rad = atanf(filteredAccelGyro[4] / filteredAccelGyro[5]);                 // Roll estimate
-  float thetaHat_acc_rad = asinf(fminf(fmaxf(filteredAccelGyro[3] / G_MPS2, -1.0f), 1.0f));  // Pitch estimate
-
-  // Using gyroscope to estimate the euler rates
-  float phiDot_rps   = (sinf(phiHat_rad) * filteredAccelGyro[1] + cosf(phiHat_rad) * filteredAccelGyro[2]) * tanf(thetaHat_rad) + filteredAccelGyro[0];  // Roll rate (rad/s)
-  float thetaDot_rps =  cosf(phiHat_rad) * filteredAccelGyro[1] - sinf(phiHat_rad) * filteredAccelGyro[2];                                               // Pitch rate (rad/s)
-
-  // Complementary filter implementation
-  phiHat_rad = COMP_FLTR_ALPHA * phiHat_acc_rad + (1.0f - COMP_FLTR_ALPHA) * (phiHat_rad + dt * phiDot_rps);          // Roll estimate
-  thetaHat_rad = COMP_FLTR_ALPHA * thetaHat_acc_rad + (1.0f - COMP_FLTR_ALPHA) * (thetaHat_rad + dt * thetaDot_rps);  // Pitch estimate
-}
