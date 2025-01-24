f47b049261f8cb523750d0c6b95aa4e9ef9ace3d
AbdoullahBougataya <sience.story@gmail.com>
2025-01-24 07:24:34 +0000

    Jan 24, 2025, 10:24:34 AM

--- Flight_controller.ino
+++ Flight_controller.ino
@@ -108,7 +108,7 @@ void complimentaryFilter(float* filteredAccelGyro, float &phiHat_rad, float &the
   float phiDot_rps   = (sinf(phiHat_rad) * filteredAccelGyro[1] + cosf(phiHat_rad) * filteredAccelGyro[2]) * tanf(thetaHat_rad) + filteredAccelGyro[0];  // Roll rate (rad/s)
   float thetaDot_rps =  cosf(phiHat_rad) * filteredAccelGyro[1] - sinf(phiHat_rad) * filteredAccelGyro[2];                                               // Pitch rate (rad/s)
 
-  // Complementary filter implementation
+  // Complementary filter implementation (Just like mixing the data from the gyroscope and the accelerometer with COMP_FLTR_ALPHA proportions)
   phiHat_rad = COMP_FLTR_ALPHA * phiHat_acc_rad + (1.0f - COMP_FLTR_ALPHA) * (phiHat_rad + dt * phiDot_rps);          // Roll estimate
   thetaHat_rad = COMP_FLTR_ALPHA * thetaHat_acc_rad + (1.0f - COMP_FLTR_ALPHA) * (thetaHat_rad + dt * thetaDot_rps);  // Pitch estimate
 }
