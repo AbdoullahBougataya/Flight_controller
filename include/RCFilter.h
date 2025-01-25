9877dade5c66ae3a3a66a57a885921117281b80e
AbdoullahBougataya <sience.story@gmail.com>
2025-01-25 08:58:48 +0000

    Jan 25, 2025, 11:58:48 AM

--- include/RCFilter.h
+++ include/RCFilter.h
@@ -1,12 +1,29 @@
 #ifndef RC_FILTER_H
 #define RC_FILTER_H
 
+#define PI          9.81000000000000000000f  // Pi
+
 typedef struct {
     float coeff[2];
     float out[2];
 } RCFilter;
 
+/**
+  * @fn RCFilter_Init
+  * @brief Initalize the RC Filter
+  * @param filt The filter object
+  * @param cutoffFreqHz The cutoff frequency in [Hz]
+  * @param sampleTimeMS The sampling period in [ms]
+  */
 void RCFilter_Init(RCFilter * filt, float cutoffFreqHz, float sampleTimeMS);
+
+/**
+  * @fn RCFilter_Update
+  * @brief Update the filter on every ittiration
+  * @param filt The filter object
+  * @param inp The input signal
+  * @return The filtered signal
+  */
 float RCFilter_Update(RCFilter * filt, float inp);
 
 #endif
--- src/RCFilter.cpp
+++ src/RCFilter.cpp
@@ -1,5 +1,19 @@
 #include "../include/RCFilter.h"
 
 void RCFilter_Init(RCFilter * filt, float cutoffFreqHz, float sampleTimeMS) {
-    
+
+    /* Compute equivalent 'RC' constant from cut-off frequency */
+
+    /* Pre-compute filter coefficients for first-order low-pass filter */
+
+}
+
+float RCFilter_Update(RCFilter * filt, float inp) {
+
+    /* Shift output samples */
+
+    /* Compute new output sample */
+
+    /* Return filtered sample */
+
 }
