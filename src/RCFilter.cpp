4684d2549dad853098202d5857ff61636baf8d04
AbdoullahBougataya <sience.story@gmail.com>
2025-01-25 09:07:46 +0000

    Jan 25, 2025, 12:07:46 PM

--- src/RCFilter.cpp
+++ src/RCFilter.cpp
@@ -7,14 +7,22 @@ void RCFilter_Init(RCFilter * filt, float cutoffFreqHz, float sampleTimeMS) {
     /* Pre-compute filter coefficients for first-order low-pass filter */
     filt->coeff[0] = (sampleTimeMS / 1000.0f) / ((sampleTimeMS / 1000.0f) + RC);
     filt->coeff[1] = RC / ((sampleTimeMS / 1000.0f) + RC);
+
+    /* Clear the outputs */
+    filt->out[0] = 0.0f;
+    filt->out[1] = 0.0f;
+
 }
 
 float RCFilter_Update(RCFilter * filt, float inp) {
 
     /* Shift output samples */
     filt->out[1] = filt->out[0];
+
     /* Compute new output sample */
+    filt->out[0] = filt->coeff[0] * inp + filt->coeff[1] * filt->out[1];
 
     /* Return filtered sample */
+    return (filt->out[0]);
 
 }
