3476a5c95040f4af8b2ccc03974250583d953114
AbdoullahBougataya <sience.story@gmail.com>
2025-01-28 06:55:01 +0000

    Jan 28, 2025, 9:55:01 AM

---
+++ include/CompFilter.h
@@ -0,0 +1,30 @@
+#ifndef COMP_FILTER_H
+#define COMP_FILTER_H
+
+#include <Arduino.h>
+#include <stdint.h>
+
+typedef struct {
+    float coeff[2];
+    float out[2];
+} RCFilter;
+
+/**
+  * @fn RCFilter_Init
+  * @brief Initalize the RC Filter
+  * @param filt The filter object
+  * @param cutoffFreqHz The cutoff frequency in [Hz]
+  * @param sampleTime The sampling period in [s]
+  */
+void RCFilter_Init(RCFilter * filt, float cutoffFreqHz, float sampleTime);
+
+/**
+  * @fn RCFilter_Update
+  * @brief Update the filter on every ittiration
+  * @param filt The filter object
+  * @param inp The input signal
+  * @return The filtered signal
+  */
+float RCFilter_Update(RCFilter * filt, float inp);
+
+#endif
