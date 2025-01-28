0620cf0edc56deb7dde74c4b47c8d9261c4977e2
AbdoullahBougataya <sience.story@gmail.com>
2025-01-28 13:26:02 +0000

    Jan 28, 2025, 4:26:02 PM

---
+++ include/PID.h
@@ -0,0 +1,14 @@
+#ifndef PID_CONTROLLER_H
+#define PID_CONTROLLER_H
+
+typedef struct {
+    // Controller gains
+    float Kp;
+    float Kd;
+    float Ki;
+
+    // Derivative low-pass filter time constant
+    float 
+}
+
+#endif
