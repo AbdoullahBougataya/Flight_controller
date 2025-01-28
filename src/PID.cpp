8b0f0c688789fdd2d6b2d59181fbc40eadf9f3ce
AbdoullahBougataya <sience.story@gmail.com>
2025-01-28 13:40:08 +0000

    Jan 28, 2025, 4:40:08 PM

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,3 +1,16 @@
 #include "../include/PID.h"
 
+void PIDController_Init(PIDController *pid) {
+    // Reset all variables
+    integrator = 0.0f;
+    prevError = 0.0f;
 
+    differentiator = 0.0f;
+    prevMeasurement = 0.0f;
+
+    out = 0.0f;
+}
+
+void PIDController_Update(PIDController *pid, float setpoint, float measurement) {
+    // Error signal
+}
