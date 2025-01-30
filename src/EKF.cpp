ea8c9050c5a5c7891cec511bad94f093d2de51eb
AbdoullahBougataya <sience.story@gmail.com>
2025-01-30 07:16:56 +0000

    Jan 30, 2025, 10:16:56 AM

--- src/EKF.cpp
+++ src/EKF.cpp
@@ -2,10 +2,18 @@
 
 void KalmanRollPitch_Init(kalmanRollPitch *kal, float PInit, float *Q, float *R) {
 
-    kal->phi_rad = 0.0f;
-    kal->theta_rad = 0.0f;
+    // Euler angles reset
+    kal->phi_rad = 0.0f; // Roll
+    kal->theta_rad = 0.0f; // pitch
 
     kal->P[0] = Pinit; kal->P[1] = 0.0f;
     kal->P[2] = 0.0f;  kal->P[3] = Pinit;
 
+    kal->Q[0] = Q[0]; kal->Q[1] = Q[1];
+
+    kal->R[0] = R[0]; kal->R[1] = R[1]; kal->R[2] = R[2];
+}
+
+void KalmanRollPitch_Predict(kalmanRollPitch *kal, float *sensorData, float T) {
+    float p = sensorData[]
 }
