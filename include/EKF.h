457713ecf921ec98832438f729805cc0249d1364
AbdoullahBougataya <sience.story@gmail.com>
2025-01-30 06:51:45 +0000

    Jan 30, 2025, 9:51:45 AM

--- include/EKF.h
+++ include/EKF.h
@@ -1,6 +1,30 @@
 #ifndef EXTENDED_KALMAN_FLTR_H
 #define EXTENDED_KALMAN_FLTR_H
 
+typedef struct {
+    // Controller gains
+    float Kp;
+    float Kd;
+    float Ki;
 
+    // Derivative low-pass filter time constant
+    float tau;
+
+    // Output limits
+    float limMin;
+    float limMax;
+
+    // Sample time (in seconds)
+    float T;
+
+    // Controller "Memory"
+    float integrator;
+    float prevError;           // Integrator
+    float differentiator;
+    float prevMeasurement;     // Differentiator
+
+    // out
+    float out;
+} extendedKalmanFilter;
 
 #endif
