4c95b8a762181e98c3ee6c1d54f3e0352bf2e047
AbdoullahBougataya <sience.story@gmail.com>
2025-02-04 10:28:16 +0000

    Feb 4, 2025, 1:28:16 PM

--- Flight_controller.ino
+++ Flight_controller.ino
@@ -10,13 +10,13 @@
       1. Implement PWM signal generation algorithms.
       2. Take instructions in real time from the RC controller.
     In progress ⏳:
-      1. Perform sensor fusion.
-      2. Implement additional signal processing for PID control.
-      3. Implement the PID control algorithms.
+      1. Implement additional signal processing for PID control.
+      2. Implement the PID control algorithms.
     Done ✅:
       1. Initialize the sensors.
       2. Calibrate the gyroscope.
       3. Filters the sensor data.
+      4. Perform sensor fusion.
            -------------------------------------------
   The flight controller code was highly inspired from various source in the internet, most notably:
     * Carbon aeronautics series on making a Quadcopter using Teensy (Arduino compatible).
@@ -27,7 +27,6 @@
 #include "./include/RCFilter.h"
 #include <Arduino_FreeRTOS.h>
 #include <math.h>
-#include <Servo.h>
 
 // Section 1: Constants & Global variables declarations.
 
@@ -35,14 +34,6 @@ BMI160 imu; // Declaring the imu object
 
 RCFilter lpFRC[6]; // Declaring the RC filter object
 
-<<<<<<< HEAD:Flight_controller.ino
-=======
-PIDController pid; // Declaring the pid object
-
-Servo motorLeftPWM;  // Left Motors PWM signal object
-Servo motorRightPWM; // Right Motors PWM signal object
-
->>>>>>> 5a8885688df3d74eb3b04e6ebd4ebd2b37fce670:Test.ino
 #define RAD2DEG                          57.29577951308232087680f  // Radians to degrees (per second)
 #define G_MPS2                            9.81000000000000000000f  // Gravitational acceleration (g)
 #define PI                                3.14159265358979323846f  // Pi
@@ -51,25 +42,15 @@ Servo motorRightPWM; // Right Motors PWM signal object
 #define STARTUP_DELAY                   100                        // 100 ms for the microcontroller to start
 #define INTERRUPT_1_MCU_PIN               2                        // The pin that receives the interrupt 1 signal from the IMU
 #define RC_LOW_PASS_FLTR_CUTOFF_5HZ       5.00000000000000000000f  // The cutoff frequency for the RC low pass filter
+#define RC_LOW_PASS_FLTR_CUTOFF_10HZ     10.00000000000000000000f  // The cutoff frequency for the RC low pass filter
 #define GYRO_CALIBRATION_SAMPLES_200    200                        // It takes 200 samples to calibrate the gyroscope
 #define COMP_FLTR_ALPHA                   0.03000000000000000000f  // Complimentary filter coefficient
-<<<<<<< HEAD:Flight_controller.ino
-=======
-#define THROTTLE                        110                        // The throttle of the motors (Between 0 and 1800)
-#define SETPOINT                          0.00000000000000000000f  // The setpoint for the PID Controller
-#define MTR_LEFT_PIN                      9                        // Motor left pin from the PDB standpoint
-#define MTR_RIGHT_PIN                    11                        // Motor right pin from the PDB standpoint
->>>>>>> 5a8885688df3d74eb3b04e6ebd4ebd2b37fce670:Test.ino
 
 const int8_t addr = 0x68; // 0x68 for SA0 connected to the ground
 
 volatile bool dataReady = false; // Sensor Data Ready ? yes:true | no:false
 
 uint8_t rslt = 0; // Define the result of the data extraction from the imu
-
-int M1 = 0, M2 = 0; // Motor throttle variables (to be later determined to the real motors)
-
-float rollPID = 0.0f;
 
 float gyroRateOffset[3] = { 0.0 }; // Gyro rates offsets
 
@@ -84,35 +65,20 @@ float thetaHat_rad = 0.0f; // Euler Pitch
 // Functions
 void complementaryFilter(float* filteredAccelGyro, float &phiHat_rad, float &thetaHat_rad, float dt, float alpha);
 void AccelGyroISR(); // This is the Interrupt Service Routine for retrieving data from the sensor
+void getSensorData(void *pvParameters); // getSensorData task
+void PIDUpdate(void *pvParameters); // PIDUpdate task
+void motorControl(void *pvParameters); // motorControl task
 
 // Section 2: Initialization & setup section.
 
 void setup() {
-<<<<<<< HEAD:Flight_controller.ino
   // initialize serial communication at 115200 bits per second:
-=======
-  /*========================================*/
-  /*     The PID controller settings        */
-  /*========================================*/
-  /*        Controller coefficients         */
-  /**/pid.Kp = 0.5f;                        //
-  /**/pid.Ki = 0.0f;                        //
-  /**/pid.Kd = 0.0f;                        //
-  /*----------------------------------------*/
-  /*Derivative low-pass filter time constant*/
-  /**/pid.tau = 1.5f;                       //
-  /*----------------------------------------*/
-  /*               Clampings                */
-  /**/pid.limMin = -90.0f;                  //
-  /**/pid.limMax =  90.0f;                  //
-  /*========================================*/
-
-  // Initialize serial communication at 115200 bits per second:
->>>>>>> 5a8885688df3d74eb3b04e6ebd4ebd2b37fce670:Test.ino
   Serial.begin(SERIAL_BANDWIDTH_115200);
   delay(STARTUP_DELAY);
 
   xTaskCreate(getSensorData, "sensorDataTask", 4096, NULL, 1, NULL);
+  xTaskCreate(PIDUpdate, "initializeAndUpdatePID", 4096, NULL, 1, NULL);
+  xTaskCreate(motorControl, "initializeAndUpdatePID", 4096, NULL, 1, NULL);
 }
 
 // Section 3: Looping and realtime processing.
@@ -142,18 +108,14 @@ void getSensorData(void *pvParameters) {
   // Everytime a pulse is received from the sensor, the AccelGyroISR() will set the dataReady to true, which will enable the code to be ran in the loop
   attachInterrupt(digitalPinToInterrupt(INTERRUPT_1_MCU_PIN), AccelGyroISR, RISING);
 
-  // Attach the motors pins to the pwm variables
-  motorLeftPWM.attach(MTR_LEFT_PIN, 1000, 2000);
-  motorRightPWM.attach(MTR_RIGHT_PIN, 1000, 2000);
-
   for (int i = 0; i < 6; i++) {
-    RCFilter_Init(&lpFRC[i], RC_LOW_PASS_FLTR_CUTOFF_5HZ, SAMPLING_PERIOD); // Initialize the RCFilter fc = 5 Hz ; Ts = 0.01 s
+    RCFilter_Init(&lpFRC[i], RC_LOW_PASS_FLTR_CUTOFF_10HZ, SAMPLING_PERIOD); // Initialize the RCFilter fc = 5 Hz ; Ts = 0.01 s
   }
 
   float gyroRateCumulativeOffset[3] = { 0.0 }; // Define a temporary variable to sum the offsets
 
   // For five seconds the gyroscope will be calibrating (make sure you put it on a flat surface)
-  Serial.println("Calibrating...");
+  Serial.print("Calibrating...");
   for (int i = 0; i < GYRO_CALIBRATION_SAMPLES_200; i++) {
 
     // Initialize sensor data arrays
@@ -182,106 +144,10 @@ void getSensorData(void *pvParameters) {
   for (byte i = 0; i < 3; i++) {
     gyroRateOffset[i] = gyroRateCumulativeOffset[i] / GYRO_CALIBRATION_SAMPLES_200;
   }
-<<<<<<< HEAD:Flight_controller.ino
 
   while(1) {
     // Checking if there is data ready in the sensor
     if (dataReady)
-=======
-
-  // Initialize the ESC
-  Serial.println("!!! Plug the battery in 10 seconds!!!");
-  delay(10000);
-  Serial.println("!!! Initializing ESCs !!!");
-  for (int i = 0; i < 10000; i++) {
-    motorLeftPWM.write(0);
-    motorRightPWM.write(0);
-    delay(1);
-  }
-}
-
-// Section 3: Looping and realtime processing.
-
-void loop() {
-
-// Checking if there is data ready in the sensor
-  if (dataReady)
-  {
-    dataReady = false; // Reseting the dataReady flag
-    // Initialize sensor data arrays
-    memset(accelGyro, 0, sizeof(accelGyro));
-    memset(accelGyroData, 0, sizeof(accelGyroData));
-
-    // Zeroing the motor throttles
-    M1 = 0;
-    M2 = 0;
-
-    // Reseting the PID
-    rollPID = 0.0f;
-
-    // Get both accel and gyro data from the BMI160
-    // Parameter accelGyro is the pointer to store the data
-    rslt = imu.getAccelGyroData(accelGyro);
-
-    // if the data is succesfully extracted
-    if (rslt == 0) {
-      // Format and offset the accelerometer data
-      offset(accelGyro, accelGyroData);
-
-      // Substract the offsets from the Gyro readings
-      for (byte i = 0; i < 3; i++) {
-        accelGyroData[i] -= gyroRateOffset[i];
-      }
-
-      for (int i = 0; i < 6; i++) {
-        accelGyroData[i] = RCFilter_Update(&lpFRC[i], accelGyroData[i]); // Update the RCFilter
-      }
-
-      /*
-        A complimentary filter is a premitive technique of sensor fusion
-        to use both the accelerometer and the gyroscope to predict the
-        euler angles (phi: roll, theta: pitch)
-      */
-      complementaryFilter(accelGyroData, phiHat_rad, thetaHat_rad, SAMPLING_PERIOD, COMP_FLTR_ALPHA); // This function transform the gyro rates and the Accelerometer angles into equivalent euler angles
-
-      phiHat_rad = 
-
-      // Update the PID Controller
-      rollPID = PIDController_Update(&pid, SETPOINT, phiHat_rad * RAD2DEG);
-
-      // Set the motor throttle from the MMA (Motor Mixing Algorithm)
-      if (rollPID >= 0) {
-        M1 = fminf(fmaxf(THROTTLE + (int)(rollPID * 10), 100), 300);
-        M2 = fminf(fmaxf(THROTTLE - (int)(rollPID * 10), 100), 300);
-      }
-      else {
-        M1 = fminf(fmaxf(THROTTLE - (int)(abs(rollPID) * 10), 100), 300);
-        M2 = fminf(fmaxf(THROTTLE + (int)(abs(rollPID) * 10), 100), 300);
-      }
-
-      /* The Code continues here... */
-
-      // Map motor Throttle to 0 to 180
-      M1 = map(M1, 0, 1800, 0, 180);
-      M2 = map(M2, 0, 1800, 0, 180);
-
-      // Write the PWM to the pins
-      motorLeftPWM.write(M1);
-      motorRightPWM.write(M2);
-
-      // Print the euler angles to the serial monitor
-      Serial.print("Angle:");
-      Serial.print(phiHat_rad * RAD2DEG);
-      Serial.print("\t PID: ");
-      Serial.print(rollPID);
-      Serial.print("\t Motor left Throttle: ");
-      Serial.print(M1);
-      Serial.print("\t Motor right Throttle: ");
-      Serial.print(M2);
-      Serial.println();
-    }
-    else
->>>>>>> 5a8885688df3d74eb3b04e6ebd4ebd2b37fce670:Test.ino
     {
       dataReady = false; // Reseting the dataReady flag
       // Initialize sensor data arrays
@@ -327,6 +193,14 @@ void loop() {
   }
 }
 
+void PIDUpdate(void *pvParameters) {
+    /* PID code goes here... */
+}
+
+void motorControl(void *pvParameters) {
+    /* Motor control code goes here... */
+}
+
 // Section 4: Function declarations.
 
 // Accelerometer and Gyroscope interrupt service routine
@@ -351,11 +225,8 @@ void complementaryFilter(float* filteredAccelGyro, float &phiHat_rad, float &the
   // Bound the values of the pitch and roll
   phiHat_rad = fminf(fmaxf(phiHat_rad, -PI), PI);
   thetaHat_rad = fminf(fmaxf(thetaHat_rad, -PI), PI);
-<<<<<<< HEAD:Flight_controller.ino
 
   // If the angle is absolutly less than 1, then it zeroes out
-=======
->>>>>>> 5a8885688df3d74eb3b04e6ebd4ebd2b37fce670:Test.ino
   if (abs(phiHat_rad * RAD2DEG) < 1) {
     phiHat_rad = 0;
   }
