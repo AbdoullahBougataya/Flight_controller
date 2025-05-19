130b16686c2760d5b44b5a3a891b28884f9375b0
AbduSOBITKHANOV <new1687ton@gmail.com>
2025-05-19 12:05:10 +0300

    Added PPM Decoder (RC)

---
+++ include/PPMDecoder.h
@@ -0,0 +1,31 @@
+#ifndef PPMDECODER_H
+#define PPMDECODER_H
+
+#include <Arduino.h>
+
+class PPMDecoder {
+public:
+    PPMDecoder(uint8_t ppmPin, uint8_t channelAmount);
+    void begin();
+    bool available();
+    unsigned int getChannelValue(uint8_t channel);
+
+private:
+    static void isrHandler();
+    static void readReceiver();
+
+    static uint8_t _ppmPin;
+    static uint8_t _channelAmount;
+    static unsigned int _rawValues[10]; // Up to 10 channels supported
+    static volatile bool _signalReady;
+    static unsigned int _channelIndex;
+    static unsigned long _lastTime;
+    static unsigned long _duration;
+
+    static constexpr unsigned int BLANK_TIME = 2100;
+    static constexpr unsigned int MIN_CHANNEL_VALUE = 1000;
+    static constexpr unsigned int MAX_CHANNEL_VALUE = 2000;
+    static constexpr unsigned int CHANNEL_VALUE_MAX_ERROR = 10;
+};
+
+#endif // PPMDECODER_H
