45514986b515c7f692c80feb109c484a78220f52
AbdoullahBougataya <sience.story@gmail.com>
2025-06-20 12:30:20 +0000

    Jun 20, 2025, 3:30:20 PM

--- src/EMA.cpp
+++ src/EMA.cpp
@@ -2,4 +2,10 @@
 
 unsigned int EMAFilter_Update(EMAFilter *filt, uint16_t remoteController)
 {
+    out[0] = out[1];
+    out[1] = out[2];
+    out[2] = out[3];
+    out[3] = out[4];
+    out[4] = remoteController;
+    return out[0] * 0.2f + out[1] * 0.2f + out[2] * 0.2f + out[3] * 0.2f + out[4] * 0.2f;
 }
