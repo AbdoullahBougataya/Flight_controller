811a958deeaddbe557218c927f1f2794a1e0c963
AbdoullahBougataya <sience.story@gmail.com>
2025-06-20 12:33:01 +0000

    Jun 20, 2025, 3:33:01 PM

--- include/EMA.h
+++ include/EMA.h
@@ -7,6 +7,6 @@ typedef struct {
     uint16_t out[5];
 } EMAFilter;
 
-uint16_t EMAFilter_Update(EMAFilter *filt, uint16_t remoteController);
+uint16_t EMAFilter_Update(EMAFilter *filt, uint16_t inp);
 
 #endif
--- src/EMA.cpp
+++ src/EMA.cpp
@@ -1,11 +1,11 @@
 #include "../include/EMA.h"
 
-unsigned int EMAFilter_Update(EMAFilter *filt, uint16_t remoteController)
+unsigned int EMAFilter_Update(EMAFilter *filt, uint16_t inp)
 {
-    out[0] = out[1];
-    out[1] = out[2];
-    out[2] = out[3];
-    out[3] = out[4];
-    out[4] = remoteController;
-    return out[0] * 0.2f + out[1] * 0.2f + out[2] * 0.2f + out[3] * 0.2f + out[4] * 0.2f;
+    EMAFilter->out[0] = EMAFilter->out[1];
+    EMAFilter->out[1] = EMAFilter->out[2];
+    EMAFilter->out[2] = EMAFilter->out[3];
+    EMAFilter->out[3] = EMAFilter->out[4];
+    EMAFilter->out[4] = inp;
+    return EMAFilter->out[0] * 0.2f + EMAFilter->out[1] * 0.2f + EMAFilter->out[2] * 0.2f + EMAFilter->out[3] * 0.2f + EMAFilter->out[4] * 0.2f;
 }
