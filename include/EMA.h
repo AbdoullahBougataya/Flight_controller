6f3b172a2f7ee49f7dfee03691f125c1e8f5974a
AbdoullahBougataya <sience.story@gmail.com>
2025-06-20 12:22:43 +0000

    Jun 20, 2025, 3:22:43 PM

--- include/EMA.h
+++ include/EMA.h
@@ -3,6 +3,10 @@
 
 #include <stdint.h>
 
-unsigned int EMAFilter_Update(EMAFilter *filt, uint16_t remoteController);
+typedef struct {
+    uint16_t out[5];
+} EMAFilter;
+
+uint16_t EMAFilter_Update(EMAFilter *filt, uint16_t remoteController);
 
 #endif
