879d653c61f6e8f409c4caa1133342a95376d3e8
AbdoullahBougataya <sience.story@gmail.com>
2025-06-20 13:03:11 +0000

    Jun 20, 2025, 4:03:11 PM

--- src/AVR.cpp
+++ src/AVR.cpp
@@ -1,11 +1,11 @@
 #include "../include/AVR.h"
 
-unsigned int AVRFilter_Update(AVRFilter *filt, uint16_t inp)
+uint16_t AVRFilter_Update(AVRFilter *filt, uint16_t inp)
 {
-    AVRFilter->out[0] = AVRFilter->out[1];
-    AVRFilter->out[1] = AVRFilter->out[2];
-    AVRFilter->out[2] = AVRFilter->out[3];
-    AVRFilter->out[3] = AVRFilter->out[4];
-    AVRFilter->out[4] = inp;
-    return AVRFilter->out[0] * 0.2f + AVRFilter->out[1] * 0.2f + AVRFilter->out[2] * 0.2f + AVRFilter->out[3] * 0.2f + AVRFilter->out[4] * 0.2f;
+    filt->out[0] = filt->out[1];
+    filt->out[1] = filt->out[2];
+    filt->out[2] = filt->out[3];
+    filt->out[3] = filt->out[4];
+    filt->out[4] = inp;
+    return filt->out[0] * 0.2f + filt->out[1] * 0.2f + filt->out[2] * 0.2f + filt->out[3] * 0.2f + filt->out[4] * 0.2f;
 }
